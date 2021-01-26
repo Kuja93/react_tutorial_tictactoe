32f5eaec1229ec1c9793782851f64d53cc6eec1c
Jonny <jonathanadonnell@msn.com>
2021-01-26 21:03:34 +0000

    Updates

---
+++ precache-manifest.c9baa35a25839876227769585f7db59b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26f457145033b61c1cba3d95f327dd7f",
+    "url": "/react_tutorial_tictactoe/index.html"
+  },
+  {
+    "revision": "ebe39db3a8050f916e4c",
+    "url": "/react_tutorial_tictactoe/static/css/main.2b160546.chunk.css"
+  },
+  {
+    "revision": "b103a757f55538ff6bd5",
+    "url": "/react_tutorial_tictactoe/static/js/2.638fca11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_tutorial_tictactoe/static/js/2.638fca11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebe39db3a8050f916e4c",
+    "url": "/react_tutorial_tictactoe/static/js/main.a130e6c2.chunk.js"
+  },
+  {
+    "revision": "6df99942d8b9a7ce7ce5",
+    "url": "/react_tutorial_tictactoe/static/js/runtime-main.0e7d21ce.js"
+  }
+]);
